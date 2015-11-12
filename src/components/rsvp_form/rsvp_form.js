bd1ba59ba2a0df35b15e0193f227af622719bb39
Tom Jenkins <tom@itsravenous.com>
2015-11-12 22:22:28 +0000

    Switch aspect ratio when rotating on android

--- src/components/rsvp_form/rsvp_form.js
+++ src/components/rsvp_form/rsvp_form.js
@@ -56,7 +56,23 @@ var RsvpForm = function (el) {
 			this.showShutter();
 			this.hideHint();
 		}.bind(this));
+
+		Webcam.on('error', function (e) {
+			alert('There seems to be a problem accessing your webcam - please refresh and select "Allow" when asked.');
+		});
 	}
+
+	// Listen for portrait/landscape change to sort webcam aspect ratio on Android
+	if (device.platform === 'Android') {
+		window.addEventListener('orientationchange', function () {
+			if (this.attached && this.el.style.display !== 'none') {
+				this.attached = false;
+				this.requestCamera(null, true);
+			}
+		}.bind(this));
+	}
+
+	this.sizeCamera();
 
 	// Bind form submit
 	this.form.addEventListener('submit', this.handleSubmit.bind(this));
@@ -138,10 +154,49 @@ RsvpForm.prototype = {
 		this.cameraResult.style.display = 'none';
 	},
 
-	requestCamera: function () {
+	showImg: function () {
+		this.cameraImg.style.display = null;
+	},
+
+	hideImg: function () {
+		this.cameraImg.style.display = 'none';
+	},
+
+	requestCamera: function (e, fromOrientationChange) {
 		if (!this.attached) {
-			Webcam.attach(this.cameraImg);
-			this.attached = true;
+			// Set portrait aspect for mobile cams
+			setTimeout(function () {
+				this.sizeCamera(fromOrientationChange);
+				Webcam.attach(this.cameraImg);
+				this.attached = true;
+			}.bind(this), 100);
+		}
+	},
+
+	sizeCamera: function (fromOrientationChange) {
+		if (device.platform === 'Android') {
+			var portrait = window.innerWidth < window.innerHeight;
+			if (fromOrientationChange) portrait = !portrait;
+			var cw = portrait ? 240 : 320;
+			var ch = portrait ? 320 : 240;
+			Webcam.reset();
+			Webcam.set({
+				width: cw,
+				height: ch,
+				destWidth: cw,
+				destHeight: ch
+			});
+			this.cameraWrapper.style.width = cw+'px';
+			this.cameraWrapper.style.height = ch+'px';
+			this.cameraResult.style.width = cw+'px';
+			this.cameraResult.style.height = ch+'px';
+			Webcam.on('live', function () {
+				var vid = this.cameraWrapper.querySelector('video');
+				vid.style.width = cw+'px';
+				vid.style.height = ch+'px';
+				vid.style.transform = null;
+				Webcam.off('live');
+			}.bind(this));
 		}
 	},
 
@@ -151,7 +206,8 @@ RsvpForm.prototype = {
 			this.cameraData.value = data_uri;
 			// Show photo
 			this.cameraResult.innerHTML = '<img src="'+data_uri+'"/>';
-			this.cameraResult.style.display = null;
+			this.showResult();
+			this.hideImg();
 			// Show reset button
 			this.showReset();
 			this.cameraShutter.style.display = 'none';
@@ -163,6 +219,7 @@ RsvpForm.prototype = {
 		this.hideResult();
 		// Hide reset button
 		this.hideReset();
+		this.showImg();
 		this.showShutter();
 	},
 
